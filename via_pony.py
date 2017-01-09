c6f2a8355813cf05fb75839fc9c9a955e1a663a6
DpoBoceka <kymaxnn@mail.ru>
2017-01-10 02:33:04 +0400

    Update via_pony.py

--- via_pony.py
+++ via_pony.py
@@ -1,7 +1,5 @@
 from pony.orm import *
 
-db = Database()
-db.bind('postgres', user='python', password='123', host='localhost', database='py_monitor')
 
 class Gath_data(db.Entity):
     id = PrimaryKey(int, auto=True)
@@ -9,15 +7,12 @@ class Gath_data(db.Entity):
     time = Required(str)
     online = Required(bool)
 
-db.generate_mapping(create_tables=True)
-
-sql_debug(True)
-
-#p1 = Gath_data(addr='http://www.google.ru', time='21:00', online=True)
-#commit()
 
 @db_session
 def adding_data(addr, time, online):
     Gath_data(addr=addr, time=time, online=online)
-    
-p1 = adding_data('http://www.google.ru', '21:00', True)
+
+db = Database()
+db.bind('postgres', user='python', password='123', host='localhost', database='py_monitor')
+db.generate_mapping(create_tables=True)
+sql_debug(True)
